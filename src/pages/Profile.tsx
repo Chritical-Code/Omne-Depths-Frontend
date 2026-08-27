3142a47959a6136730673790cc0bb662f46993dc
chritical <christopherkbanks@gmail.com>
2026-08-27 12:39:36 -0400

    Laid foundation for adding ai generated topics.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,28 @@
+import type { Topic, TopicData } from "@/types/types";
+
 export default function Profile(){
+    function handleClick(){
+        console.log("clicky");
+        loadTopics();
+    }
+    
     return(
-        <div className="flex h-full w-full">
-            <p className="ml-2">Profile</p>
+        <div className="flex flex-col items-center h-full w-full">
+            <p className="">Profile</p>
+            <button className="btn w-25 h-15" onClick={() => handleClick()}>Generate Topics</button>
         </div>
     );
+}
+
+//fetch topics from backend
+async function loadTopics(){
+    const response = await fetch("http://localhost:8000/generatetopics/");
+    const topicData: TopicData = await response.json();
+
+    let topics: Topic[] = []
+    topicData.results.forEach((topicDatum) => {
+        topics.push(topicDatum);
+    })
+    
+    console.log(topics);
 }
